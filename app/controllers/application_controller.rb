1be05745ac1827898440ca4be73b49c26138a46e
Ruslan Zekokh <zekokh@icloud.com>
2017-04-14 12:10:06 +0300

    new pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :user_signed_in?
   helper_method :current_user
-  after_filter :cors_set_access_control_headers
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -13,10 +12,4 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_path, notice: "Пожалуйста, пройдите авторизацию!"
   end
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
 end
