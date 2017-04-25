3eab30c02f59b271dc469bf879a9e66f3f0761ae
Ruslan Zekokh <zekokh@icloud.com>
2017-04-25 19:47:20 +0300

    new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :user_signed_in?
+  #before_action :user_signed_in?
   helper_method :current_user
   helper_method :current_user_id
 
